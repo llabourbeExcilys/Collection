f714461505bc2f8a614be35c4f4bbd9f4994ccfb
llabourbeExcilys <llabourbe@excilys.com>
2020-04-08 14:59:47 +0200

    Début du projet avec nuxt, spring rest et swagger

---
+++ back/src/test/java/com/excilys/loic/collection/CollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excilys.loic.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,6 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    port: 8080, // par défaut: 3000
+    host: '0.0.0.0' // par défaut: localhost
+  },
   mode: 'universal',
   /*
   ** Headers of the page
