7c77166faf839457f0d9194ed990695861b2aaf8
llabourbeExcilys <llabourbe@excilys.com>
2020-06-12 16:05:31 +0200

    Réarangement de la grille flex lors de la modification

--- front/src/components/MangaSerie.vue
+++ front/src/components/MangaSerie.vue
@@ -2,120 +2,143 @@
 	<div>
 		<v-card>
 			<v-container fluid>
-				<v-row dense :align="edit ? 'baseline' : 'center'" justify="center" no-gutters>
-					<v-col :cols="4">
-						<v-card-title>
-							<v-text-field v-if="edit" v-model="item.title" outlined label="Titre"></v-text-field>
-							<div v-else>{{ item.title }}</div>
-						</v-card-title>
-						<v-card-subtitle>
-							<v-text-field v-if="edit" v-model="item.editor" outlined dense label="Edition"></v-text-field>
-							<div v-else>{{ item.edition }} ({{ item.editor }})</div>
-						</v-card-subtitle>
-					</v-col>
-					<v-col :cols="2">
-						<div v-if="edit">
+				<template v-if="edit">
+					<v-row align="stretch" justify="space-around" no-gutters>
+						<v-col :cols="5">
+							<v-text-field v-model="item.title" outlined label="Titre"></v-text-field>
+						</v-col>
+						<v-col :cols="3">
 							<v-text-field v-model="item.autor" outlined label="Auteur"></v-text-field>
-							<v-row align="start">
-								<v-col :cols="6">
-									<v-text-field
-										v-model="item.owned"
-										v-if="edit"
-										outlined
-										dense
-										label="Possédé"
-										type="number"
-										@input="item.owned = parseInt($event)"
-									></v-text-field
-								></v-col>
-								<v-col :cols="6">
-									<v-text-field
-										v-model="item.published"
-										v-if="edit"
-										outlined
-										dense
-										label="Publié"
-										type="number"
-										@input="item.published = parseInt($event)"
-									></v-text-field
-								></v-col>
+						</v-col>
+						<v-col :cols="2">
+							<v-text-field v-model="item.editor" outlined label="Edition"></v-text-field>
+						</v-col>
+						<v-col :cols="1">
+							<v-row align="center" justify="center">
+								<v-col :cols="5">
+									<v-tooltip :disabled="show" top>
+										<template v-slot:activator="{ on }">
+											<v-btn v-on="on" icon @click="show = !show">
+												<v-icon large>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+											</v-btn>
+										</template>
+										<span>Afficher les détails</span>
+									</v-tooltip>
+								</v-col>
+								<v-col :cols="5">
+									<v-btn @click="clickEdit" icon>
+										<v-icon>{{ 'mdi-pencil-off' }}</v-icon>
+									</v-btn>
+								</v-col>
 							</v-row>
-						</div>
-						<div v-else>{{ item.autor }}</div>
-					</v-col>
+						</v-col>
+					</v-row>
 
-					<v-col :cols="1">
-						<v-chip
-							v-if="!edit"
-							class="ma-2"
-							:color="item.owned === item.published ? 'green' : 'orange'"
-							text-color="white"
-						>
-							{{ item.owned }} / {{ item.published }}
-						</v-chip>
-					</v-col>
-					<v-col :cols="1">
-						<div v-if="edit">
+					<v-row align="baseline" justify="space-around" no-gutters>
+						<v-col :cols="2">
+							<v-text-field
+								v-model="item.owned"
+								v-if="edit"
+								outlined
+								label="Possédé"
+								type="number"
+								@input="item.owned = parseInt($event)"
+							>
+							</v-text-field>
+						</v-col>
+						<v-col :cols="2">
+							<v-text-field
+								v-model="item.published"
+								v-if="edit"
+								outlined
+								label="Publié"
+								type="number"
+								@input="item.published = parseInt($event)"
+							></v-text-field>
+						</v-col>
+						<v-col :cols="2">
 							<v-select
 								v-model="item.type"
 								outlined
 								:items="possibleTypes"
-								:rules="[v => !!v || 'Le genre est requis']"
-								label="Genre"
+								:rules="[v => !!v || 'Le type est requis']"
+								label="Type"
 								required
 							></v-select>
-						</div>
-						<div v-else>{{ item.type }}</div>
-					</v-col>
-					<v-col :cols="3">
-						<v-row no-gutters>
-							<v-col>
-								<div v-if="edit">
-									<v-combobox
-										v-model="item.genres"
-										:items="possibleGenres"
-										label="Genres"
-										multiple
-										chips
-									></v-combobox>
-								</div>
-								<div v-else>
-									<v-chip
-										v-for="genre in item.genres"
-										v-bind:key="genre"
-										class="ma-2"
-										small
-										dense
-										color="grey lighten-1"
-										text-color="grey darken-4"
-									>
-										{{ genre }}
-									</v-chip>
-								</div>
-							</v-col>
-						</v-row>
-					</v-col>
-					<v-col>
-						<v-card-actions>
-							<v-tooltip :disabled="show" top>
-								<template v-slot:activator="{ on }">
-									<v-btn v-on="on" icon @click="show = !show">
-										<v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-									</v-btn>
-								</template>
-								<span>Afficher les détails</span>
-							</v-tooltip>
-							<v-tooltip v-if="show" :disabled="edit" top>
-								<template v-slot:activator="{ on }">
-									<v-btn v-on="on" @click="clickEdit" icon>
-										<v-icon small>{{ edit ? 'mdi-pencil-off' : 'mdi-pencil' }}</v-icon>
-									</v-btn>
-								</template>
-								<span>Editer l'élément</span>
-							</v-tooltip>
-						</v-card-actions>
-					</v-col>
-				</v-row>
+						</v-col>
+						<v-col :cols="5">
+							<v-combobox
+								v-model="item.genres"
+								:items="possibleGenres"
+								label="Genres"
+								chips
+								outlined
+								multiple
+								small-chips
+							></v-combobox>
+						</v-col>
+					</v-row>
+				</template>
+
+				<template v-else>
+					<v-row dense align="center" justify="center" no-gutters>
+						<v-col :cols="4">
+							<v-card-title>
+								{{ item.title }}
+							</v-card-title>
+							<v-card-subtitle> {{ item.edition }} ({{ item.editor }}) </v-card-subtitle>
+						</v-col>
+						<v-col :cols="2">
+							{{ item.autor }}
+						</v-col>
+						<v-col :cols="1">
+							<v-chip
+								class="ma-2"
+								:color="item.owned === item.published ? 'green' : 'orange'"
+								text-color="white"
+							>
+								{{ item.owned }} / {{ item.published }}
+							</v-chip>
+						</v-col>
+						<v-col :cols="1">
+							{{ item.type }}
+						</v-col>
+						<v-col :cols="3">
+							<v-chip
+								v-for="genre in item.genres"
+								v-bind:key="genre"
+								class="ma-2"
+								small
+								dense
+								color="grey lighten-1"
+								text-color="grey darken-4"
+							>
+								{{ genre }}
+							</v-chip>
+						</v-col>
+						<v-col>
+							<v-card-actions>
+								<v-tooltip :disabled="show" top>
+									<template v-slot:activator="{ on }">
+										<v-btn v-on="on" icon @click="show = !show">
+											<v-icon large>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+										</v-btn>
+									</template>
+									<span>Afficher les détails</span>
+								</v-tooltip>
+								<v-tooltip v-if="show" :disabled="edit" top>
+									<template v-slot:activator="{ on }">
+										<v-btn v-on="on" @click="clickEdit" icon>
+											<v-icon>{{ 'mdi-pencil' }}</v-icon>
+										</v-btn>
+									</template>
+									<span>Editer l'élément</span>
+								</v-tooltip>
+							</v-card-actions>
+						</v-col>
+					</v-row>
+				</template>
+
 				<v-row v-if="show" dense align="center" justify="center">
 					<v-divider />
 				</v-row>
